78f6781fcf712efa7b35a42264bf0bc97b95cb3f
BillM123 <BillM@spooky.ghost>
2024-05-09 22:35:50 +0300

    Return of the edgelist (frfr)

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "prototypes.h"
 #include "datatypes.h"
+#define MAX_EDGES 12000
 
 void bfs(struct Graph graph,int S,int D,int V,int *spLength, int *numOfsps){
     int visited[V] ;
@@ -38,6 +39,8 @@ void bfs(struct Graph graph,int S,int D,int V,int *spLength, int *numOfsps){
 int main() {
     int numVertices = 0;
     int num1 = 0, num2 = 0;
+    char blank;
+    struct node* edgeList[MAX_EDGES];
     char filename[] = "edgelists/Erdos.edgelist";
     
     FILE *file = fopen(filename, "r");
@@ -49,10 +52,10 @@ int main() {
     fscanf(file, "%d\n", &numVertices);
     struct Graph *graph = createAGraph(numVertices);
 
-    while(fscanf(file,"%d %d\n", &num1, &num2)) {
+    for(int i = 0; fscanf(file, "%d %c%d\n", &num1, &blank, &num2); i++) {
         // Process the numbers here
         printf("%d %d\n", num1, num2);
-        addEdge(graph, num1, num2);
+        edgeList[i] = addEdge(graph, num1, num2);
     }
     printf("%d %d\n", num1, num2);
     fclose(file);
