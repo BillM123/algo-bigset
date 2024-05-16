6fe3137194299e179be1d993aba751b5d62511b6
starcatcher <fishingunderthelantern@spooky.ghost>
2024-05-16 19:28:01 +0300

    Added sorting functions

--- main.c
+++ main.c
@@ -5,6 +5,73 @@
 #include "include/datatypes.h"
 #define MAX_EDGES 12000
 
+//https://www.geeksforgeeks.org/merge-sort/
+// Merges two subarrays of arr[].
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(struct Edge **arr, int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    // Create temp arrays
+    struct Edge *L[n1], *R[n2];
+
+    // Copy data to temp arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        *L[i] = *arr[l + i];
+    for (j = 0; j < n2; j++)
+        *R[j] = *arr[m + 1 + j];
+
+    // Merge the temp arrays back into arr[l..r
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i]->vertex1->accessCounter + L[i]->vertex2->accessCounter 
+        <= R[j]->vertex1->accessCounter + R[j]->vertex2->accessCounter) {
+            *arr[k] = *L[i];
+            i++;
+        }
+        else {
+            *arr[k] = *R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[],
+    // if there are any
+    while (i < n1) {
+        *arr[k] = *L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[],
+    // if there are any
+    while (j < n2) {
+        *arr[k] = *R[j];
+        j++;
+        k++;
+    }
+}
+
+// l is for left index and r is right index of the
+// sub-array of arr to be sorted
+void mergeSort(struct Edge **arr, int l, int r)
+{
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
 
 //https://www.geeksforgeeks.org/binomial-coefficient-dp-9/
 // Returns value of Binomial Coefficient C(n, k)
